ffede728eab2d0c91ed2f285fcb6676198b668fd
Assaf Lavie <me@assaflavie.com>
2015-06-08 18:06:00 +0300

    Updated setup.py for registration with Pypi

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ setup(
     name='runlike',
     version='0.1',
     py_modules=['runlike'],
+    description='Reverse-engineer docker run command line arguments based on running containers',
+    author='Assaf Lavie',
+    author_email='a@assaflavie.com',
+    url='https://github.com/assaflavie/runlike/',
+    download_url = 'https://github.com/assaflavie/runlike/tarball/0.1',
+    keywords=['docker', 'cli'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
         runlike=runlike:cli
-    ''',
+    '''
 )
