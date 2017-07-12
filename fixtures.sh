da5cdffb74376acb049d8c29106a1af2040a52ef
Assaf Lavie <me@assaflavie.com>
2017-07-12 09:04:27 +0300

    some actual testing...

--- fixtures.sh
+++ fixtures.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker build -t runlike_fixture dockerfiles/
 docker rm -f runlike_fixture
-docker run -d --name runlike_fixture runlike_fixture
+docker run -d --name runlike_fixture \
+    -p 400:400 \
+    -v $(pwd):/workdir \
+    -v /random_volume \
+    runlike_fixture
--- test_runlike.py
+++ test_runlike.py
@@ -1,11 +1,26 @@
+import unittest
+import os
 from runlike import Inspector
 from subprocess import check_output, STDOUT, CalledProcessError
 
 
-def test_command_empty():
-    check_output("./fixtures.sh")
-    ins = Inspector("runlike_fixture", True, False)
-    ins.inspect()
-    print ins.format_cli()
+class TestInspection(unittest.TestCase):
+
+    def setUp(self):
+        check_output("./fixtures.sh")
+        ins = Inspector("runlike_fixture", True, False)
+        ins.inspect()
+        ins.pretty = True
+        self.output = ins.format_cli()
+
+    def expect_substr(self, substr):
+        self.assertIn(substr, self.output)
+
+    def test_ports(self):
+        self.expect_substr("-p 0.0.0.0:400:400/tcp")
+
+    def test_volumes(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        self.expect_substr("--volume=\"%s:/workdir\"" % cur_dir)
 
 
