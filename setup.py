652f960f6fa0d82267f6a448ffffff4fa974b754
Assaf Lavie <me@assaflavie.com>
2017-07-12 21:31:16 +0300

    up version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup
 
 setup(
     name='runlike',
-    version='0.1.4',
+    version='0.2.0',
     py_modules=['runlike'],
     description='Reverse-engineer docker run command line arguments based on running containers',
     author='Assaf Lavie',
     author_email='a@assaflavie.com',
     url='https://github.com/assaflavie/runlike/',
-    download_url = 'https://github.com/assaflavie/runlike/tarball/0.1.2',
-    keywords=['docker', 'cli'],
+    download_url='https://github.com/assaflavie/runlike/tarball/0.2.0',
+    keywords=[
+        'docker',
+        'cli'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
         runlike=runlike:cli
-    '''
-)
+    ''')
