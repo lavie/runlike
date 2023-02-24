840032f476d495f1c71caac249d3dc658073d5cd
reconman <reconman@users.noreply.github.com>
2023-02-24 21:35:40 +0100

    Fix b" output prefix

--- runlike/runlike.py
+++ runlike/runlike.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import click
+import sys
 
 try:
     from .inspector import Inspector
@@ -10,7 +11,7 @@ except ValueError:
 
 @click.command(
     help="Shows command line necessary to run copy of existing Docker container.")
-@click.argument("container", required=False )
+@click.argument("container", required=False)
 @click.option(
     "--no-name",
     is_flag=True,
@@ -20,17 +21,16 @@ except ValueError:
 def cli(container, no_name, pretty, stdin):
 
     # TODO: -i, -t, -d as added options that override the inspection
-    if container:
+    if container or stdin:
         ins = Inspector(container, no_name, pretty)
-        ins.inspect()
-        print(ins.format_cli().encode(encoding='utf8'))
-    elif stdin:
-        ins = Inspector()
-        ins.pretty = pretty
-        raw_json = click.get_text_stream('stdin').read()
-        ins.set_container_facts(raw_json)
-        print(ins.format_cli())
-    else: 
+        if container:
+            ins.inspect()
+        elif stdin:
+            raw_json = click.get_text_stream('stdin').read()
+            ins.set_container_facts(raw_json)
+
+        sys.stdout.buffer.write(ins.format_cli().encode(encoding='utf8'))
+    else:
         raise click.UsageError("usage error")
 
 def main():
